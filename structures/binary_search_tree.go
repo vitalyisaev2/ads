938c4adf462ab3ca1e22a2c5223fd33d9e8dacfe
Vitaly Isaev <vitalyisaev2@gmail.com>
2016-12-04 23:17:41 +0300

    Broken

--- structures/binary_search_tree.go
+++ structures/binary_search_tree.go
@@ -3,87 +3,145 @@
 
 package structures
 
-import (
-	"sync"
-)
+import
+//"fmt"
+
+"sync"
 
 // BinarySearchTreeItem describes interfaces for the BinarySearchTree payload
 type BinarySearchTreeItem interface {
-	Less(BinarySearchTreeItem) bool
-	Equal(BinarySearchTreeItem) bool
+	Less(interface{}) bool
+	Equal(interface{}) bool
 }
 
 // BinarySearchTree is a simple implementation of a binary search tree
 type BinarySearchTree interface {
-	Search(BinarySearchTreeItem) BinarySearchTreeNode
-	//Insert(BinarySearchTreeItem)
+	//fmt.Stringer
+	Search(BinarySearchTreeItem) BinarySearchTreeItem
+	Insert(BinarySearchTreeItem)
 	Min() BinarySearchTreeItem
 	//Max() BinarySearchTreeItem
-	//Traverse() []BinarySearchTreeItem
+	Items() []BinarySearchTreeItem
+	Root() BinarySearchTreeItem
 	Len() int
 }
 
 type binarySearchTreeImpl struct {
 	sync.RWMutex
-	root *binarySearchTreeNodeImpl
+	root *binarySearchTreeNode
 	size int
 }
 
-func (tree *binarySearchTreeImpl) Search(sought BinarySearchTreeItem) BinarySearchTreeNode {
+func (tree *binarySearchTreeImpl) Insert(newItem BinarySearchTreeItem) {
+	tree.Lock()
+	defer tree.Unlock()
+	tree.root.insert(newItem)
+	tree.size++
+}
+
+func (tree *binarySearchTreeImpl) Search(soughtItem BinarySearchTreeItem) BinarySearchTreeItem {
 	tree.RLock()
 	defer tree.RUnlock()
-	return tree.root.search(sought)
+	return tree.root.search(soughtItem)
 }
+
 func (tree *binarySearchTreeImpl) Min() BinarySearchTreeItem {
 	tree.RLock()
 	defer tree.RUnlock()
 	return tree.root.min()
 }
+
 func (tree *binarySearchTreeImpl) Len() int {
 	tree.RLock()
 	defer tree.RUnlock()
 	return tree.size
 }
 
-// BinarySearchTreeNode represents node of a binary search tree
-type BinarySearchTreeNode interface {
-	Item() BinarySearchTreeItem
-	search(BinarySearchTreeItem) BinarySearchTreeNode
-	min() BinarySearchTreeItem
+//func (tree *binarySearchTreeImpl) String() string {
+//tree.RLock()
+//defer tree.RUnlock()
+//return fmt.Sprintf("Tree: total items %d", tree.size)
+//}
+
+func (tree *binarySearchTreeImpl) Items() []BinarySearchTreeItem {
+	tree.RLock()
+	defer tree.RUnlock()
+	items := make([]BinarySearchTreeItem, 0, tree.size)
+	tree.root.traverseSlice(&items)
+	return items
 }
 
-type binarySearchTreeNodeImpl struct {
+func (tree *binarySearchTreeImpl) Root() BinarySearchTreeItem {
+	tree.RLock()
+	defer tree.RUnlock()
+	return tree.root.item
+}
+
+type binarySearchTreeNode struct {
 	item   BinarySearchTreeItem
-	parent *binarySearchTreeNodeImpl
-	left   *binarySearchTreeNodeImpl
-	right  *binarySearchTreeNodeImpl
+	parent *binarySearchTreeNode
+	left   *binarySearchTreeNode
+	right  *binarySearchTreeNode
 }
 
-func (node *binarySearchTreeNodeImpl) Item() BinarySearchTreeItem { return node.item }
+func (node *binarySearchTreeNode) Item() BinarySearchTreeItem { return node.item }
 
-func (node *binarySearchTreeNodeImpl) search(sought BinarySearchTreeItem) BinarySearchTreeNode {
+func (node *binarySearchTreeNode) insert(newItem BinarySearchTreeItem) {
+	if node.item == nil {
+		node.item = newItem
+		return
+	}
+
+	if node.item.Equal(newItem) {
+		return
+	}
+
+	if node.item.Less(newItem) {
+		if node.right == nil {
+			newNode := &binarySearchTreeNode{
+				item:   newItem,
+				parent: node,
+			}
+			node.right = newNode
+			return
+		}
+		node.right.insert(newItem)
+	}
+
+	if node.left == nil {
+		newNode := &binarySearchTreeNode{
+			item:   newItem,
+			parent: node,
+		}
+		node.left = newNode
+		return
+	}
+	node.left.insert(newItem)
+}
+
+func (node *binarySearchTreeNode) search(soughtItem BinarySearchTreeItem) BinarySearchTreeItem {
 	if node.item == nil {
 		return nil
 	}
 
-	if node.item.Equal(sought) {
-		return node
+	if node.item.Equal(soughtItem) {
+		return node.item
 	}
 
-	if node.item.Less(sought) {
+	if node.item.Less(soughtItem) {
 		if node.right != nil {
-			return node.right.search(sought)
+			return node.right.search(soughtItem)
 		}
 	} else {
 		if node.left != nil {
-			return node.left.search(sought)
+			return node.left.search(soughtItem)
 		}
 	}
 
 	return nil
 }
 
-func (node *binarySearchTreeNodeImpl) min() BinarySearchTreeItem {
+func (node *binarySearchTreeNode) min() BinarySearchTreeItem {
 	minNode := node
 	for minNode.left != nil {
 		minNode = minNode.left
@@ -91,9 +149,17 @@ func (node *binarySearchTreeNodeImpl) min() BinarySearchTreeItem {
 	return minNode.item
 }
 
+func (node *binarySearchTreeNode) traverseSlice(items *[]BinarySearchTreeItem) {
+	if node.item != nil {
+		node.left.traverseSlice(items)
+		*items = append(*items, node.item)
+		node.right.traverseSlice(items)
+	}
+}
+
 // NewBinarySearchTree returns empty binary search tree
 func NewBinarySearchTree() BinarySearchTree {
-	root := &binarySearchTreeNodeImpl{}
+	root := &binarySearchTreeNode{}
 	tree := &binarySearchTreeImpl{root: root, size: 0}
 	return tree
 }
--- structures/binary_search_tree_test.go
+++ structures/binary_search_tree_test.go
@@ -1,16 +1,38 @@
 package structures
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestBinaryTree(t *testing.T) {
-	var (
-		tree BinarySearchTree
-	)
+type binaryTreeString struct {
+	value string
+}
 
-	tree = NewBinarySearchTree()
-	assert.Nil(t, tree.Min())
+func (thisItem *binaryTreeString) Less(thatItem interface{}) bool {
+	return thisItem.value < thatItem.(*binaryTreeString).value
+}
+
+func (thisItem *binaryTreeString) Equal(thatItem interface{}) bool {
+	return thisItem.value == thatItem.(*binaryTreeString).value
+}
+
+func TestBinaryTree(t *testing.T) {
+	payload := []binaryTreeString{
+		binaryTreeString{"a"},
+		binaryTreeString{"b"},
+		binaryTreeString{"c"},
+	}
+
+	tree := NewBinarySearchTree()
+	for i := range payload {
+		tree.Insert(&payload[i])
+	}
+	assert.Equal(t, tree.Len(), 3)
+	fmt.Println(tree)
+	fmt.Println(tree.Items())
+	assert.True(t, tree.Min().Equal(&binaryTreeString{"a"}))
+	assert.True(t, tree.Root().Equal(&binaryTreeString{"a"}))
 }
