4323e4e42745adf60f737047a9143c242bea3dc2
Vitaly Isaev <vitalyisaev2@gmail.com>
2018-09-11 00:46:10 +0300

    B-3 | Not operational

--- graph/directed_cyclic_graph.go
+++ graph/directed_cyclic_graph.go
@@ -1,1 +1,40 @@
 package graph
+
+import "math"
+
+var _ DirectedCyclicGraph = (*defaultDirectedGraph)(nil)
+
+type defaultDirectedCyclicGraph struct {
+	defaultDirectedGraph
+}
+
+// DijkstraShortestPathes returns shortest pathes between
+func (g *defaultDirectedCyclicGraph) DijkstraShortestPathes(from Node) (map[Node][]Node, error) {
+
+	// check node existence
+	if _, exists := g.nodes[from.ID()]; !exists {
+		return nil, errNodeDoesNotExist(from)
+	}
+
+	// initialize shortest path with initial values
+	shortest := make(map[NodeID]EdgeWeight, len(g.nodes))
+	for _, node := range g.nodes {
+		if node.ID() != from.ID() {
+			shortest[node.ID()] = math.Inf(1)
+		} else {
+			shortest[node.ID()] = 0
+		}
+	}
+
+	// initialize mapping for the preceding nodes, lying on the shortest path
+	pred := make(map[NodeID]NodeID)
+
+	// for node in Q:
+}
+
+// NewDirectedCyclicGraph returns DirectedCyclicGraph
+func NewDirectedCyclicGraph() DirectedCyclicGraph {
+	return &defaultDirectedCyclicGraph{
+		defaultDirectedGraph: newDirectedGraph(),
+	}
+}
