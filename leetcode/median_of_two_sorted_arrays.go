2f8502be647c2335a8e47950aaa7faabc700df94
Vitaly Isaev <vitalyisaev2@gmail.com>
2019-01-05 02:16:02 +0300

    median of two sorted arrays: finally failed to do the job

--- leetcode/median_of_two_sorted_arrays.go
+++ leetcode/median_of_two_sorted_arrays.go
@@ -1,21 +1,114 @@
 package leetcode
 
+import (
+	"fmt"
+	"math"
+)
+
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 	if len(nums1) == 0 && len(nums2) == 0 {
 		panic("invalid input")
 	}
 
 	if len(nums1) == 0 {
-		panic("impl")
-		//return findMedianSortedArray(nums2)
+		return findMedianSortedArray(nums2)
 	}
 
 	if len(nums2) == 0 {
-		panic("impl")
-		//return findMedianSortedArray(nums1)
+		return findMedianSortedArray(nums1)
 	}
 
-	return 0
+	if len(nums1) == 1 && len(nums2) == 1 {
+		return (float64(minIntegers(nums1[0], nums2[0])) + float64(maxIntegers(nums1[0], nums2[0]))) / 2
+	}
+
+	var (
+		total = len(nums1) + len(nums2)
+
+		arr1 = newArray(nums1)
+		arr2 = newArray(nums2)
+
+		inter1 = arr1.currentInterval()
+		inter2 = arr2.currentInterval()
+
+		firstOrd  = inter1.compare(inter2)
+		actualOrd = firstOrd
+	)
+
+	for {
+		if arr1.currentInterval().intersects(arr2.currentInterval()) {
+			if total%2 == 0 && arr1.size()+arr2.size() < total/2 {
+				right1 := arr1.rightItem()
+				right2 := arr2.rightItem()
+				if right1 > right2 {
+					arr2.index += 1
+				} else {
+					arr1.index += 1
+				}
+			}
+
+			if total%2 != 0 && arr1.size()+arr2.size() > total/2 {
+				left1 := arr1.leftItem()
+				left2 := arr2.leftItem()
+				if left1 > left2 {
+					arr1.index -= 1
+				} else {
+					arr2.index -= 1
+				}
+			}
+
+			break
+		}
+
+		switch actualOrd {
+		case less:
+			if !arr1.onRightBorder() {
+				inter1 = arr1.shiftIndexRight()
+			}
+			inter2 = arr2.shiftIndexLeft()
+		case greater:
+			inter1 = arr1.shiftIndexLeft()
+			if arr1.currentInterval().intersects(arr2.currentInterval()) && arr1.size()+arr2.size() == total/2 {
+				break
+			}
+			inter2 = arr2.shiftIndexRight()
+		case equal:
+			break
+		default:
+			panic("unexpected ord value")
+		}
+
+		if actualOrd = inter1.compare(inter2); actualOrd != firstOrd {
+			break
+		}
+	}
+
+	fmt.Println(">")
+	fmt.Println("arr1", arr1)
+	fmt.Println("arr2", arr2)
+	fmt.Println(">")
+
+	// even number of elements
+	if total%2 == 0 {
+		right1 := arr1.rightItem()
+		right2 := arr2.rightItem()
+		right := minIntegers(right1, right2)
+
+		left1 := arr1.leftItem()
+		left2 := arr2.leftItem()
+		left := maxIntegers(left1, left2)
+
+		fmt.Println("left1", left1, "left2", left2, "right1", right1, "right2", right2)
+
+		return (float64(left) + float64(right)) / 2
+	}
+
+	// odd number of elements
+	right1 := arr1.rightItem()
+	right2 := arr2.rightItem()
+	fmt.Println("right1", right1, "right2", right2)
+	return float64(minIntegers(right1, right2))
+
 }
 
 // array - helper struct that contains array of numbers
@@ -27,19 +120,7 @@ type array struct {
 	nums  []int
 }
 
-/*
-func shiftBorderRight(ixIn, end int) (ixOut, startOut int) {
-	ixOut = middle(ixIn+1, end)
-	startOut = ixIn
-	return
-}
-
-func shiftBorderLeft(ixIn, start int) (ixOut, endOut int) {
-	ixOut = middle(start, ixIn)
-	endOut = ixIn
-	return
-}
-*/
+func (a array) size() int { return a.index + 1 }
 
 func (a array) currentInterval() interval {
 	if a.index == -1 {
@@ -47,7 +128,7 @@ func (a array) currentInterval() interval {
 			right:  a.nums[0],
 			border: true,
 		}
-	} else if a.index == len(a.nums) {
+	} else if a.index == len(a.nums)-1 {
 		return interval{
 			left:   a.nums[len(a.nums)-1],
 			border: true,
@@ -65,27 +146,68 @@ func (a *array) shiftIndexRight() interval {
 		newStart int
 		result   interval
 	)
-	if newIndex == len(a.nums) {
+
+	if a.index == len(a.nums)-1 {
 		// right border reached
 		panic("right border reached")
-	} else if newIndex == len(a.nums)-1 {
-		// last item before border
-		newIndex = len(a.nums)
-		newStart = a.index
-		result = interval{left: a.nums[a.index], border: true}
-	} else {
-		// common item
+	}
+
+	if a.end-a.start > 1 {
 		newIndex = middle(a.index+1, a.end)
-		newStart = a.index
+	} else {
+		newIndex = a.end
+	}
+
+	if newIndex == len(a.nums)-1 {
+		// last item before border
+		result = interval{left: a.nums[newIndex], border: true}
+	} else {
 		result = interval{left: a.nums[newIndex], right: a.nums[newIndex+1]}
 	}
 
+	newStart = a.index + 1
 	a.index = newIndex
 	a.start = newStart
 	return result
 }
 
+func (a *array) shiftIndexLeft() interval {
+	var (
+		newIndex int
+		newEnd   int
+		result   interval
+	)
+
+	if a.index == -1 {
+		// left border reached
+		panic("left border reached")
+	}
+
+	if a.index == 0 {
+		// last item before border
+		result = interval{right: a.nums[newIndex], border: true}
+		newIndex = -1
+	} else {
+
+		if a.end-a.start > 1 {
+			newIndex = middle(a.start, a.index)
+		} else {
+			newIndex = a.start
+		}
+		result = interval{left: a.nums[newIndex], right: a.nums[newIndex+1]}
+	}
+
+	newEnd = a.index
+	a.index = newIndex
+	a.end = newEnd
+	return result
+}
+
 func middle(start, stop int) int {
+	if start == stop {
+		return start
+	}
+
 	span := stop - start
 	if span%2 == 0 {
 		return start + span/2 - 1
@@ -93,10 +215,36 @@ func middle(start, stop int) int {
 	return start + span/2
 }
 
+func (a array) leftItem() int {
+	left := int(math.MinInt64)
+	if a.index >= 0 {
+		left = a.nums[a.index]
+	}
+	return left
+}
+
+func (a array) rightItem() int {
+	right := int(math.MaxInt64)
+	if a.index < 0 {
+		right = a.nums[0]
+	} else if a.index < len(a.nums)-1 {
+		right = a.nums[a.index+1]
+	}
+	return right
+}
+
+func (a array) onRightBorder() bool { return a.index == len(a.nums)-1 }
+
+func (a array) onLeftBorder() bool { return a.index == -1 }
+
+func (a array) String() string {
+	return fmt.Sprintf("start: %d, index: %d, end: %d", a.start, a.index, a.end)
+}
+
 func newArray(nums []int) *array {
 	result := &array{
 		start: 0,
-		end:   len(nums),
+		end:   len(nums) - 1,
 		nums:  nums,
 	}
 	result.index = middle(result.start, result.end)
@@ -109,18 +257,42 @@ type interval struct {
 	border bool
 }
 
-func (i interval) compare(other interval) ord {
-	if i.left < other.left {
+func (i interval) compare(j interval) ord {
+	if i.left < j.left {
 		return less
 	}
-	if i.left > other.left {
+	if i.left > j.left {
 		return greater
 	}
 	return equal
 }
 
-func (i interval) intersects(other interval) bool {
-	return i.left <= other.left && other.left <= i.right
+func (i interval) intersects(j interval) bool {
+	if i.border && j.border {
+		return intervalBorderBorderIntersection(i, j) || intervalBorderBorderIntersection(j, i)
+	}
+	if i.border || j.border {
+		return intervalMiddleBorderIntersection(i, j) || intervalMiddleBorderIntersection(j, i)
+	}
+	return intervalMiddleMiddleIntersection(i, j) || intervalMiddleMiddleIntersection(j, i)
+}
+
+func intervalBorderBorderIntersection(i, j interval) bool {
+	return i.left <= j.right
+}
+
+func intervalMiddleBorderIntersection(i, j interval) bool {
+	return i.left <= j.left && j.left <= j.right ||
+		j.left <= j.right && j.right <= i.right
+}
+
+func intervalMiddleMiddleIntersection(i, j interval) bool {
+	return (i.left <= j.left && j.left <= i.right && i.right <= j.left) ||
+		(i.left <= j.left && j.left <= j.right && j.right <= i.right)
+}
+
+func (i interval) String() string {
+	return fmt.Sprintf("left: %d, right: %d, border: %v", i.left, i.right, i.border)
 }
 
 type ord int8
@@ -130,153 +302,6 @@ const (
 	equal   ord = 0
 	greater ord = 1
 )
-
-/*
-func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-
-	if len(nums1) == 0 && len(nums2) == 0 {
-		panic("invalid input")
-	}
-
-	if len(nums1) == 0 {
-		return findMedianSortedArray(nums2)
-	}
-
-	if len(nums2) == 0 {
-		return findMedianSortedArray(nums1)
-	}
-
-	var (
-		total          = len(nums1) + len(nums2)
-		half           = total / 2
-		start1, start2 = 0, 0
-		end1, end2     = len(nums1), len(nums2)
-		ix1            = middle(start1, end1)
-		ix2            = middle(start2, end2)
-	)
-
-	var (
-		firstRelation  = compareIntegers(nums1[ix1], nums2[ix2])
-		actualRelation = firstRelation
-	)
-	fmt.Println(">")
-	fmt.Println("start1", start1, "ix1", ix1, "end1", end1)
-	fmt.Println("start2", start2, "ix2", ix2, "end2", end2)
-	fmt.Println(">")
-
-	for {
-		if actualRelation == less {
-
-			budget := len(nums1) - ix1 - 1 + ix2
-			fmt.Println("less", budget)
-			if budget <= 0 {
-				fmt.Println("e6")
-				break
-			}
-			ix1, start1 = shiftBorderRight(ix1, end1)
-			ix2, end2 = shiftBorderLeft(ix2, start2)
-		} else if actualRelation == greater {
-			budget := len(nums2) - ix2 - 1 + ix1
-			fmt.Println("greater", budget)
-			if budget <= 0 {
-				fmt.Println("e7")
-				break
-			}
-			ix1, end1 = shiftBorderLeft(ix1, start1)
-			ix2, start2 = shiftBorderRight(ix2, end2)
-		} else {
-			if total == 2 {
-				ix2 -= 1
-			}
-			fmt.Println("e1")
-			break
-		}
-
-		fmt.Println(">")
-		fmt.Println("start1", start1, "ix1", ix1, "end1", end1)
-		fmt.Println("start2", start2, "ix2", ix2, "end2", end2)
-		fmt.Println(">")
-
-		// case when the whole half of sample belongs to the only array
-		if ix1 < 0 {
-			ix2 = half - 1
-			fmt.Println("e4")
-			break
-		} else if ix2 < 0 {
-			ix1 = half - 1
-			fmt.Println("e5")
-			break
-		}
-
-		if actualRelation = compareIntegers(nums1[ix1], nums2[ix2]); actualRelation != firstRelation {
-			break
-		}
-
-	}
-
-	// border is determined
-	fmt.Println("ix", ix1, ix2)
-
-	// lookup for next value to count median
-
-	// even number of elements
-	if total%2 == 0 {
-		right1 := rightBorder(nums1, ix1)
-		right2 := rightBorder(nums2, ix2)
-		right := minIntegers(right1, right2)
-
-		left1 := leftBorder(nums1, ix1)
-		left2 := leftBorder(nums2, ix2)
-		left := maxIntegers(left1, left2)
-
-		fmt.Println("left", left, "right", right)
-
-		return (float64(left) + float64(right)) / 2
-	}
-
-	// odd number of elements
-	right1 := rightBorder(nums1, ix1)
-	right2 := rightBorder(nums2, ix2)
-	return float64(minIntegers(right1, right2))
-}
-
-func middle(start, stop int) int {
-	span := stop - start
-	if span%2 == 0 {
-		return start + span/2 - 1
-	}
-	return start + span/2
-}
-
-func shiftBorderRight(ixIn, end int) (ixOut, startOut int) {
-	ixOut = middle(ixIn+1, end)
-	startOut = ixIn
-	return
-}
-
-func shiftBorderLeft(ixIn, start int) (ixOut, endOut int) {
-	ixOut = middle(start, ixIn)
-	endOut = ixIn
-	return
-}
-
-type relation int8
-
-const (
-	less    relation = -1
-	equal   relation = 0
-	greater relation = 1
-)
-
-func compareIntegers(a, b int) relation {
-	if a < b {
-		return less
-	}
-	if a > b {
-		return greater
-	}
-	return equal
-}
 
 func minIntegers(a, b int) int {
 	if a < b {
@@ -290,24 +315,6 @@ func maxIntegers(a, b int) int {
 		return a
 	}
 	return b
-}
-
-func rightBorder(nums []int, ix int) int {
-	right := int(math.MaxInt64)
-	if ix < 0 {
-		right = nums[0]
-	} else if ix < len(nums)-1 {
-		right = nums[ix+1]
-	}
-	return right
-}
-
-func leftBorder(nums []int, ix int) int {
-	left := int(math.MinInt64)
-	if ix >= 0 {
-		left = nums[ix]
-	}
-	return left
 }
 
 func findMedianSortedArray(nums []int) float64 {
@@ -327,4 +334,3 @@ func findMedianSortedArray(nums []int) float64 {
 
 	return float64(nums[ix])
 }
-*/
--- leetcode/median_of_two_sorted_arrays_test.go
+++ leetcode/median_of_two_sorted_arrays_test.go
@@ -1,13 +1,14 @@
 package leetcode
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-/*
 func Test_findMedianSortedArrays(t *testing.T) {
+	t.Skip() // was not able to finish this task :(
 	type testcase struct {
 		answer float64
 		num1   []int
@@ -15,9 +16,9 @@ func Test_findMedianSortedArrays(t *testing.T) {
 	}
 
 	cases := []testcase{
+		{4.5, []int{2, 3, 4, 5}, []int{1, 6, 7, 8}},
 		{4.5, []int{1, 2, 3, 4}, []int{5, 6, 7, 8}},
 		{4.5, []int{5, 6, 7, 8}, []int{1, 2, 3, 4}},
-		{4.5, []int{2, 3, 4, 5}, []int{1, 6, 7, 8}},
 		{4.5, []int{1, 6, 7, 8}, []int{2, 3, 4, 5}},
 		{2.5, []int{1, 2}, []int{3, 4}},
 		{2.0, []int{1, 3}, []int{2}},
@@ -29,6 +30,9 @@ func Test_findMedianSortedArrays(t *testing.T) {
 		{1.5, []int{1, 2}, []int{1, 2}},
 		{3.0, []int{1, 2}, []int{3, 4, 5}},
 		{3.5, []int{1, 2}, []int{3, 4, 5, 6}},
+		{2.0, []int{1}, []int{2, 3}},
+		{2.0, []int{3}, []int{1, 2}},
+		{2.0, []int{1, 2}, []int{3}},
 	}
 
 	for _, c := range cases {
@@ -36,7 +40,6 @@ func Test_findMedianSortedArrays(t *testing.T) {
 		assert.Equal(t, c.answer, findMedianSortedArrays(c.num1, c.num2), "input: %v %v", c.num1, c.num2)
 	}
 }
-*/
 
 func Test_findMedianSortedArrays_arrayShiftIndexRight(t *testing.T) {
 	nums := []int{0, 1, 2, 3, 4, 5, 6, 7}
@@ -44,21 +47,131 @@ func Test_findMedianSortedArrays_arrayShiftIndexRight(t *testing.T) {
 	array := newArray(nums)
 	inter := array.currentInterval()
 	assert.Equal(t, 3, array.index)
+	assert.Equal(t, 0, array.start)
+	assert.Equal(t, 7, array.end)
 	assert.Equal(t, 3, inter.left)
 	assert.Equal(t, 4, inter.right)
+	assert.False(t, inter.border)
 
 	inter = array.shiftIndexRight()
 	assert.Equal(t, 5, array.index)
+	assert.Equal(t, 4, array.start)
+	assert.Equal(t, 7, array.end)
 	assert.Equal(t, 5, inter.left)
 	assert.Equal(t, 6, inter.right)
+	assert.False(t, inter.border)
 
 	inter = array.shiftIndexRight()
 	assert.Equal(t, 6, array.index)
+	assert.Equal(t, 6, array.start)
+	assert.Equal(t, 7, array.end)
 	assert.Equal(t, 6, inter.left)
 	assert.Equal(t, 7, inter.right)
+	assert.False(t, inter.border)
 
 	inter = array.shiftIndexRight()
-	assert.Equal(t, 6, array.index)
-	assert.Equal(t, 6, inter.left)
-	assert.Equal(t, 7, inter.right)
+	assert.Equal(t, 7, array.index)
+	assert.Equal(t, 7, array.start)
+	assert.Equal(t, 7, array.end)
+	assert.Equal(t, 7, inter.left)
+	assert.True(t, inter.border)
+
+	assert.Panics(t, func() { _ = array.shiftIndexRight() })
+}
+
+func Test_findMedianSortedArrays_arrayShiftIndexLeft(t *testing.T) {
+	nums := []int{0, 1, 2, 3, 4, 5, 6, 7}
+
+	array := newArray(nums)
+	inter := array.currentInterval()
+	assert.Equal(t, 3, array.index)
+	assert.Equal(t, 0, array.start)
+	assert.Equal(t, 7, array.end)
+	assert.Equal(t, 3, inter.left)
+	assert.Equal(t, 4, inter.right)
+	assert.False(t, inter.border)
+
+	inter = array.shiftIndexLeft()
+	assert.Equal(t, 1, array.index)
+	assert.Equal(t, 0, array.start)
+	assert.Equal(t, 3, array.end)
+	assert.Equal(t, 1, inter.left)
+	assert.Equal(t, 2, inter.right)
+	assert.False(t, inter.border)
+
+	inter = array.shiftIndexLeft()
+	assert.Equal(t, 0, array.index)
+	assert.Equal(t, 0, array.start)
+	assert.Equal(t, 1, array.end)
+	assert.Equal(t, 0, inter.left)
+	assert.Equal(t, 1, inter.right)
+	assert.False(t, inter.border)
+
+	inter = array.shiftIndexLeft()
+	assert.Equal(t, -1, array.index)
+	assert.Equal(t, 0, array.start)
+	assert.Equal(t, 0, array.end)
+	assert.Equal(t, 0, inter.left)
+	assert.True(t, inter.border)
+
+	assert.Panics(t, func() { _ = array.shiftIndexLeft() })
+}
+
+func Test_findMedianSortedArrays_arrayShiftComplex(t *testing.T) {
+	nums := []int{
+		0, 1, 2, 3, 4, 5, 6, 7,
+		8, 9, 10, 11, 12, 13, 14, 15}
+
+	array := newArray(nums)
+
+	inter := array.currentInterval()
+	assert.Equal(t, 7, array.index)
+	assert.Equal(t, 0, array.start)
+	assert.Equal(t, 15, array.end)
+	assert.Equal(t, 7, inter.left)
+	assert.Equal(t, 8, inter.right)
+
+	inter = array.shiftIndexRight()
+	assert.Equal(t, 11, array.index)
+	assert.Equal(t, 8, array.start)
+	assert.Equal(t, 15, array.end)
+	assert.Equal(t, 11, inter.left)
+	assert.Equal(t, 12, inter.right)
+
+	inter = array.shiftIndexLeft()
+	assert.Equal(t, 9, array.index)
+	assert.Equal(t, 8, array.start)
+	assert.Equal(t, 11, array.end)
+	assert.Equal(t, 9, inter.left)
+	assert.Equal(t, 10, inter.right)
+
+	inter = array.shiftIndexLeft()
+	assert.Equal(t, 8, array.index)
+	assert.Equal(t, 8, array.start)
+	assert.Equal(t, 9, array.end)
+	assert.Equal(t, 8, inter.left)
+	assert.Equal(t, 9, inter.right)
+
+}
+
+func Test_findMedianSortedArrays_newArray(t *testing.T) {
+	nums := []int{2, 3, 4}
+	array := newArray(nums)
+	assert.Equal(t, 0, array.index)
+	assert.Equal(t, 0, array.start)
+	assert.Equal(t, 2, array.end)
+}
+
+func Test_findMedianSortedArrays_intersections0(t *testing.T) {
+	var inter1, inter2 interval
+
+	inter1 = interval{left: 1, right: 2}
+	inter2 = interval{left: 3, right: 4}
+	assert.False(t, inter1.intersects(inter2))
+	assert.False(t, inter2.intersects(inter1))
+
+	inter1 = interval{left: 1, border: true}
+	inter2 = interval{left: 2, right: 3}
+	assert.True(t, inter1.intersects(inter2))
+	assert.True(t, inter2.intersects(inter1))
 }
